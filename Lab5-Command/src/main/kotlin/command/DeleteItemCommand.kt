3eaf5dff9123ff37d3d562d4035539b765ad3503
IvanAndreyshev-NOTEBOOK <vanyha360@yandex.ru>
2018-03-30 07:35:03 +0300

    Скелет команды удаления

--- Lab5-Command/src/main/kotlin/command/DeleteItemCommand.kt
+++ Lab5-Command/src/main/kotlin/command/DeleteItemCommand.kt
@@ -1,6 +1,7 @@
 package command
 
 import document.IDocumentItem
+import document.IImage
 import io.IImageFileManager
 
 class DeleteItemCommand(
@@ -8,14 +9,48 @@ class DeleteItemCommand(
         private val items: MutableList<IDocumentItem>,
         private val position: Int
 ) : ICommand {
-    private val item = items[position]
+    private val item: IDocumentItem = items[position]
+    private var mBehavior: ICommand
 
-    override fun execute() {
+    init {
+        val image = item.image
+
+        mBehavior = when {
+            item.paragraph != null -> DeleteParagraph()
+            image != null -> DeleteImage(image)
+            else -> throw NotImplementedError()
+        }
     }
 
-    override fun undo() {
+    override fun execute() = mBehavior.execute()
+
+    override fun undo() = mBehavior.undo()
+
+    override fun destroy() = mBehavior.destroy()
+
+    inner class DeleteParagraph : ICommand {
+        override fun execute() {
+            items.removeAt(position)
+        }
+
+        override fun undo() {
+            items.add(position, item)
+        }
     }
 
-    override fun destroy() {
+    inner class DeleteImage(private val image: IImage) : ICommand {
+        override fun execute() {
+            fileManager.markImageOnDelete(image.path, true)
+            items.removeAt(position)
+        }
+
+        override fun undo() {
+            fileManager.markImageOnDelete(image.path, false)
+            items.add(position, item)
+        }
+
+        override fun destroy() {
+            fileManager.markImageOnDelete(image.path, false)
+        }
     }
 }
